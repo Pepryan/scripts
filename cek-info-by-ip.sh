cdf0a36eb19fc38f92b65c16efc6d679c0769363
Febryan Ramadhan <febryan@boer.co.id>
2023-06-27 03:31:45 +0000

    Updated instance_info

--- cek-info-by-ip.sh
+++ cek-info-by-ip.sh
@@ -9,7 +9,9 @@ ip_list=($(cat ip.txt))
 # LoopIP
 for ip in "${ip_list[@]}"
 do
-    instance_info=$(openstack server show $(openstack server list --project $project_name --ip "$ip" -f value -c ID) -f json)
+    # instance_info=$(openstack server show $(openstack server list --project $project_name --ip "$ip" -f value -c ID) -f json)
+    instance_info=$(openstack server show $(openstack server list --project $project_name -f value | grep -w "$ip" | awk -F' ' '{print $1}') -f json)
+    
 
     instance_id=$(echo "$instance_info" | jq -r '.id')
     instance_name=$(echo "$instance_info" | jq -r '.name')
@@ -26,7 +28,7 @@ do
     ram_gb=$((ram/1024))
     vcpus=$(echo "$flavor_info" | jq -r '.vcpus')
 
-    # Output
+    # Menampilkan hasil
     # echo "IP $ip, Instance ID $instance_id, Instance Name $instance_name, Floating IP $floating_ip, $vcpus, $ram_gb"
     echo $ip, $instance_id, $instance_name, $floating_ip, $vcpus, $ram_gb"G"
     # echo "External IP: $external_ip"
